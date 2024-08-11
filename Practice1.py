b517826fca3fe23e2db3bef793d37089305f823c
RajaDevi0512 <rajadevi0512@gmail.com>
2024-08-11 18:23:02 +0530

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,69 @@
+#writing my first python code
+# print("Hello! World")
+# print("My Python World!")
+"""""
+multiline
+comments can be 
+given here using "
+"""""
+
+'''
+multiline
+comments can be 
+given here using
+'''
+'''
+language = 'python is my world!_ = + ] } \ |'
+print (language)
+print(type(language))
+print(len(language))
+'''
+'''
+myvalue = "Prasanna"
+print(myvalue[2:6]) # prints 2,3,4,5
+print(myvalue[::-1]) #reversing a string
+ '''
+'''
+#Palindrome
+user_input = "MalayalaM"
+reversed_string = user_input[::-1]
+
+if user_input == reversed_string:
+    print("the given string is palindrome")                        
+else:                
+    print("the given string not palindrome")
+'''
+#Numeric datatype
+# x = 1 #integer
+# y = 5.8 #float
+# z = 2j #complex
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# #Type casitng
+# x = int(1) #integer
+# y = float(5) #float
+# z = complex(2) #complex
+
+# print(x)
+# print(y)
+# print(z)
+#len() function
+# mystring = "Magizhini"
+# print(len(mystring))
+
+# mylist = ["apple","banana","cherry"]
+# print(len(mylist))
+# num = 13
+# if num > 0:
+#     print("positive")
+#     print(num)
+# num = int(input())
+# if(num%2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+# user_input = input()
+# print(type(user_input))
